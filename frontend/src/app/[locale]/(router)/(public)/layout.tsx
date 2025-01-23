20144b180ba689c7e3ef4bc475886ad5f40d7cd6
Cristian Dizeo <dizeoc@gmail.com>
2025-01-23 19:51:35 -0300

    create public layout with Nav and Footer

---
+++ frontend/src/app/[locale]/(router)/(public)/layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen bg-lightpink">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
