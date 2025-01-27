79ea8919662cde45aec9b1329b4668a271144363
Cristian Dizeo <dizeoc@gmail.com>
2025-01-27 12:44:31 -0300

    fix footer color

--- frontend/src/app/[locale]/(router)/(public)/components/Footer.tsx
+++ frontend/src/app/[locale]/(router)/(public)/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-deepblue text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Vocaltech. Todos los derechos reservados.
