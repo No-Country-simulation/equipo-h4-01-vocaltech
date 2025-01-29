774a941db696dc5cec1e379bcb91b7a72879327b
Cristian Dizeo <dizeoc@gmail.com>
2025-01-28 23:47:41 -0300

    add favicon

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { useTranslation } from 'react-i18next';
 import type { Metadata } from "next";
 import { notFound } from 'next/navigation';
 import i18nConfig from '@/i18n/config/i18nConfig';
@@ -7,26 +6,33 @@ import { dir } from 'i18next';
 import "./styles/globals.css";
 import ThemeProvider from "@/provider/ThemeProvider/ThemeProvider";
 
-const metadata: Metadata = {
-  title: "Vocal Tech - In construction",
+export const metadata: Metadata = {
+  title: "VocalTech",
   description: "Transmití tus ideas, transformá con tu voz",
+  icons: {
+    icon: "/img/favicon.ico",
+    shortcut: "/img/favicon.ico",
+    apple: "/img/favicon.ico",
+  },
 };
 
 export function generateStaticParams() {
   return i18nConfig.locales.map(locale => ({ locale }));
 }
 
-export default async function RootLayout(props: Readonly<{
+export default async function RootLayout({
+  children,
+  params,
+}: {
   children: ReactNode;
-  params: Promise<{ locale: string }>;
-}>) {
-
-  const { locale } = await props.params;
-  const { children } = props;
+  params: { locale: string };
+}) {
+  const { locale } = params;
 
   if (!i18nConfig.locales.includes(locale)) {
     notFound();
   }
+
   return (
     <html lang={locale} dir={dir(locale)} suppressHydrationWarning>
       <body>
