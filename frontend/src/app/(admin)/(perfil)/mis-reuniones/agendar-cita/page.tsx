5021cd3dcfb05f7075837160282218eda23e3b21
Cristian Dizeo <dizeoc@gmail.com>
2025-02-11 02:07:09 -0300

    fix agendar cita

--- frontend/src/app/(admin)/(perfil)/mis-reuniones/agendar-cita/page.tsx
+++ frontend/src/app/(admin)/(perfil)/mis-reuniones/agendar-cita/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import StepsDate from './components/StepsDate'
-import { ChevronDown } from 'lucide-react'
 
 export default async function page() {
-  await new Promise(resolve => setTimeout(resolve, 3500));
+  
   return (
-    <div>
+    <div className='col-span-full'>
       <StepsDate />
     </div>
   )
