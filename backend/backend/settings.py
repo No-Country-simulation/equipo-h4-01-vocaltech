c0aafd834dc2c628b89a7a612e8d3766dc6a3fc6
Leandro Sánchez <leanmsan@gmail.com>
2025-02-12 10:44:57 -0300

    Corrección de errores
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,16 +142,16 @@ TEMPLATES = [
 AUTH_USER_MODEL = "auth_service.User"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASSWORD"),
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),
+#     }
+# }
 
 DATABASES = {
     "default": {
