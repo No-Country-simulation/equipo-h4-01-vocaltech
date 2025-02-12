15c06b85a56555a14ed96a63600419e8da9a5253
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2025-02-12 13:57:19 -0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,9 +97,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "*",
-]
+CSRF_TRUSTED_ALL_ORIGINS = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
